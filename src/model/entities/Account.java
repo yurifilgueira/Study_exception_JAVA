425cb7609ca278e9bbc89799ec594f8d31d5389b
Yuri Filgueira <yurimedeiros141@gmail.com>
2022-08-27 18:30:43 -0300

    Initial implementation

---
+++ src/model/entities/Account.java
@@ -0,0 +1,45 @@
+package model.entities;
+
+public class Account {
+
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}	
+	
+	public void deposit(Double depositValue) {
+		balance += depositValue;
+	}
+	
+	public void withDraw(Double withDrawValue) {
+		balance -= withDrawValue;
+	}
+	
+}
